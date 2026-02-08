2568553a8f284a7ca459d76b5739f7284e947e01
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-08 14:44:02 +0530

    Add variables.py

---
+++ basics/variables.py
@@ -0,0 +1,14 @@
+# Variables exaamples
+name = "geetheswar"  # string
+age = 20             # integer
+height = 5.8         # float
+is_student = True    # boolean
+
+print("Name:", name)
+print("Age:", age)
+print("Height:",height)
+print("Is_student:",is_student)
+
+# Multiple assignment
+a, b, c = 10, 20, 30
+print("values:", a, b, c)
