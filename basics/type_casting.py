15dbaa864c85cd9c225609fb28187fb2a906d892
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-09 20:12:31 +0530

    Add type_casting.py

---
+++ basics/type_casting.py
@@ -0,0 +1,16 @@
+# Typing cating examples
+
+# converting string to integer
+num_str ="20"
+num_int = int(num_str)
+print("String to int:"num_int,)
+
+# converting integer to float
+num = 10
+decimal = float(num)
+print("Integer to float:",decimal) 
+
+# converting integer to string
+age = 20
+string = str(age)
+print("integer to string:",string) 
