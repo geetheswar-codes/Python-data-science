e904894d6767eecad7156d8c2d6134aac3e5a6f3
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-09 20:34:53 +0530

    Update arithmetic.py

--- basics/arithmetic.py
+++ basics/arithmetic.py
@@ -1,7 +1,7 @@
 # Arithmecit operations
 
-a = int(input("Enter a number:")
-b = int(input("Enter a number:")
+a = int(input("Enter a number:"))
+b = int(input("Enter a number:"))
 print("Addition",a+b)
 print("Subtraction",a-b)
 print("Multiplication",a*b)
