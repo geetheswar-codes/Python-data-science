f66be9be7136752cceaf939599227bd4143c02bc
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-11 22:11:59 +0530

    Add calculator.py

---
+++ basics/calculator.py
@@ -0,0 +1,27 @@
+# Simple calculator
+a = float(input("Enter First number:"))
+b = float(input("Enter second number:"))
+
+print("choose operation:")
+
+print(" 1. Addition")
+print(" 2. Subtracion")
+print(" 3. Multiplication")
+print(" 4. Division")
+
+choice = int(input("Enetr choice (1-4):"))
+
+if choice == 1:
+  result = a+b
+elif choice == 2:
+  result = a - b
+elif choice == 3:
+  result = a * b
+elif choice == 4:
+  if b != 0:
+    result = a / b
+  else:
+    result ="cannot divide by zero"
+else:
+  result = "invalid choice"
+print("Result:",result)
