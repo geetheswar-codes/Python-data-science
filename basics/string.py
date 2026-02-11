310d37bc13164cd9db42b08b9cdbc6df2084e53f
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-11 21:47:53 +0530

    Add string.py

---
+++ basics/string.py
@@ -0,0 +1,18 @@
+# String example
+text = "python programming"
+print("Original text:", text)
+
+# string length
+print("Length:",len(text))
+
+# Converting to uppercase
+print("uppercase:",text.upper())
+
+# coverting to lower case
+print("lower case:",text.lower())
+
+#Access first character
+print("first character:",text[1])
+
+# string slicing:
+print("First 6 characters:",text[:7])
