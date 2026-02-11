9c345d21357e413239e7f5eb99074bc82628db4f
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-11 21:26:31 +0530

    Add lists.py

---
+++ basics/lists.py
@@ -0,0 +1,15 @@
+# List  example
+#Creating a list
+fruits = ["Apple","Banana","Mango"]
+print("Fruit list:", fruits)
+
+# Accesssing elements in list
+print("First fruit:",fruits[0])
+
+# Adding new elements in list
+fruits.append("Orange")
+print("After adding:",fruits)
+
+# removing elements in list
+fruits.remove("Banana")
+print("After removing:",fruits)
