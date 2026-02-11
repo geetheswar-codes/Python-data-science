ab5dfcca0720b9fae9bc6627074e5c8397699f58
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-11 20:58:09 +0530

    Add function.py

---
+++ basics/function.py
@@ -0,0 +1,13 @@
+# Function example
+# defining a function
+def great():
+  print("Hello, welcome to Python!")
+
+great() # Calling the function 
+
+# Function with parameters
+def add_numbers(a, b):
+  result = a + b
+  print("Sum:", result)
+  
+add_numbers(5, 3) 
