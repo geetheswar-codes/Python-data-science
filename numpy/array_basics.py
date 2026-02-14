915b67f8478b2e028fb412935d52506f55447fbc
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-14 21:44:20 +0530

    Add array_basics.py

---
+++ numpy/array_basics.py
@@ -0,0 +1,12 @@
+# Numpy array basics
+import numpy as np
+
+# creating a array
+numbers = np.array([1,2,3,4,5])
+print("Array:",numbers)
+print("type:",type(numbers))
+print("Shape:",numbers.shape)
+
+# Basic operations on numpy array
+print("Add 10:",numbers+10)
+print("Multiply by 2:", numbers*2)
