3f0b3741b9773eda6c8bb60f8bbcef3aeb23a0e9
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-16 20:41:46 +0530

    Add array_statistics.py

---
+++ numpy/array_statistics.py
@@ -0,0 +1,10 @@
+#numpy array statistics
+import numpy as np
+
+arr = np.array([10 , 20 , 30 , 40 , 50 ])
+
+   print("array:",arr)
+   print("sum:",np.sum(arr))
+   print("mean:",np.mean(arr))
+   print("maximum:",np.max(arr))
+   print("minimum:",np.min(arr))
