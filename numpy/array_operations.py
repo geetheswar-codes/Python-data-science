2e069f7ba9213c671c6d6ddee8e3ca174dfbd303
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-16 20:47:31 +0530

    Add array_operations.py

---
+++ numpy/array_operations.py
@@ -0,0 +1,9 @@
+#  Numpy array operations
+import numpy as np
+arr = np.array([2,4,6,8])
+print("Original array:",arr)
+
+# basic operations 
+print(" Add 5:",arr+5)
+print("Mulitiply by 3:",arr*3)
+print("square:",arr**2)
