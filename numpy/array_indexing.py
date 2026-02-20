bea340a384a52b64e15b51364361c4ff3937f69c
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-20 20:19:36 +0530

    Add array_indexing.py

---
+++ numpy/array_indexing.py
@@ -0,0 +1,12 @@
+# array indexing and slicing
+import numpy as np
+arr = np.array([10, 20, 30, 40, 50])
+print("Original Array:",arr)
+
+# Indexing
+print("first element:",arr[0])
+print("last element:",arr[-1])
+
+# slicing
+print("first three elements:",arr[:3])
+print("elements from index 2:",arr[2:])
