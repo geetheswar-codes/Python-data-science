8c91d810eeaefa2cf0d65a02e61f7b0b9a5d8ba4
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-20 20:35:22 +0530

    Add random_array.py

---
+++ numpy/random_array.py
@@ -0,0 +1,9 @@
+# Random arrays
+import numpy as np
+# random number between 0 and 1
+rand_arr = np.random.rand(5)
+print("Random array:",rand_arr)
+
+# random integers
+rand_int = np.random.randint(1,10, size = 5)
+print("random integer:",rand_int)
