3479a7f28ef4b607bbd71feafaf1c1dd2112e4f0
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-20 20:12:04 +0530

    Add array_2d.py

---
+++ numpy/array_2d.py
@@ -0,0 +1,9 @@
+# 2D  Array example
+import numpy as np
+
+# creating a 2D array (matrix)
+matrix = np.array([[1,2,3],
+                   [4,5,6]])
+print("Matrix:",matrix)
+print("shape:", matrix.shape)
+print("number of dimesions:",matrix.ndim)
