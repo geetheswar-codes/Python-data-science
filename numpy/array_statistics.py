8d686f48fb1771b71132ae84f12083a541170145
Geetheswar <geetheswardampanaboyina@gmail.com>
2026-02-20 20:38:58 +0530

    Update array_statistics.py

--- numpy/array_statistics.py
+++ numpy/array_statistics.py
@@ -3,8 +3,8 @@ import numpy as np
 
 arr = np.array([10 , 20 , 30 , 40 , 50 ])
 
-   print("array:",arr)
-   print("sum:",np.sum(arr))
-   print("mean:",np.mean(arr))
-   print("maximum:",np.max(arr))
-   print("minimum:",np.min(arr))
+print("array:",arr)
+print("sum:",np.sum(arr))
+print("mean:",np.mean(arr))
+print("maximum:",np.max(arr))
+print("minimum:",np.min(arr))
